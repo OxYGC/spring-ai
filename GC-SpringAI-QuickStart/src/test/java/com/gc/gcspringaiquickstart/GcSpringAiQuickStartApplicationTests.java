e80874d391db131865d17b26c1bdad6cf3243534
keygenee <keygenee@gmail.com>
2026-05-06 22:51:56 +0800

    GC-SpringAI-QuickStart

---
+++ GC-SpringAI-QuickStart/src/test/java/com/gc/gcspringaiquickstart/GcSpringAiQuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gc.gcspringaiquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcSpringAiQuickStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
